97ca72e2b7f2c631b3bfff68ac455706e9ca79ff
Tobi Olajide <tobiolajide887@yahoo.com>
2023-03-15 16:05:56 +0000

    moved assets to public

--- src/components/pages/BookNow.jsx
+++ src/components/pages/BookNow.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Banner from "../../assets/images/banner.png";
 
 export default function BookNow() {
   return (
     <div >
-        <img src={Banner} className="banner" alt="raised hands in a night club"/>
+        <img src="/assets/images/banner.png" className="banner" alt="raised hands in a night club"/>
         <h1>
         Where would you like your night out to be?
         </h1>
