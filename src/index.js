a18454dbd30d6993d37f6f596cf7caf0a4aac4bf
bertiemoogle <bertiemoogle@gmail.com>
2023-03-16 14:32:39 +0000

    Contact.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Contact from './src/components/pages/Contact.jsx';
+import Contact from './components/pages/Contact.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
