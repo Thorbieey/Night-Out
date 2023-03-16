d2ae948637fd903fe7676589b49abc4e96eec3ec
bertiemoogle <bertiemoogle@gmail.com>
2023-03-16 10:37:25 +0000

    Contact page initial commit. Contact page CSS added.

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useRef } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {MdOutlineEmail} from "react-icons/md";
+import {RiMessengerLine} from "react-icons/ri";
+import {RxTwitterLogo} from "react-icons/rx";
+import emailjs from "emailjs-com";
 
-export default function Contact() {
+// Contact page with functioning contact form using Emailjs. The phone number isn't my real number - I don't want that to be accessible to people I don't know.
+
+
+function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_grn2mnp', 'template_fatenmg', form.current, 'CzMfSI5AOChpY2l85')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
-    <div >
-        <h1>
-            CONTACT
-        </h1>
+    <div>
+      <h1 className="headerFont">Contact</h1>
+      <div className="container contact-container">
+        <div className="contact-options">
+          <article classname="contact-option">
+            <MdOutlineEmail className="contact-icon"/>
+            <h4>Email</h4>
+            <h5>info@nightout.com</h5>
+            <a href="mailto:bertiemoogle@gmail.com" target="_blank" rel="noreferrer" className="contactDetails">Email Us</a>
+          </article>
+          <article classname="contact-option">
+            <RiMessengerLine className="contact-icon"/>
+            <h4>Messenger</h4>
+            <h5>Night Out</h5>
+            <a href="https://m.me/night.out" target="_blank" rel="noreferrer" className="contactDetails">Message Us</a>
+          </article>
+          <article classname="contact-option">
+            <RxTwitterLogo className="contact-icon"/>
+            <h4>Twitter</h4>
+            <h5>@NightOut</h5>
+            <a href="https://twitter.com/NightOut" target="_blank" rel="noreferrer" className="contactDetails">Tweet Us</a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your full name..." required />
+          <input type="email" name="email" placeholder="Your email..." required />
+          <textarea name="message" rows="10" placeholder="Your message here..." required></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
+        </form>
+      </div>
     </div>
-    );
+  );
 }
+
+export default Contact;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Contact from './src/components/pages/Contact.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App tab="booknow" />
+    <Contact tab="contact" />
   </React.StrictMode>
 );
 
