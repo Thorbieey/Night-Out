4ef17d3a9aa175dd582fba302533192e331b1a64
Tobi Olajide <tobiolajide887@yahoo.com>
2023-03-22 00:59:31 +0000

    updated navbar colours

--- client/src/components/navigation/Navbar.jsx
+++ client/src/components/navigation/Navbar.jsx
@@ -2,42 +2,54 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon, StarIcon } from '@heroicons/react/24/outline'
 import { NavLink } from 'react-router-dom';
+// import Logo from '../../../public/assets/images/logo/night-out-logo.png';
+
+const logo = [
+  {
+    name: 'night-out',
+    des: 'website logo',
+    imageUrl:
+      '/assets/images/logo/night-out-logo.png',
+   
+  }
+]
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-cyan-600 shadow">
+    <Disclosure as="nav" className="bg-amber-100 shadow">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-8xl px-2 sm:px-2 lg:px-2">
             <div className="flex h-16 justify-between">
               <div className="flex">
                 <div className="flex flex-shrink-0 items-center">
-                  <h1 className="inline-flex items-center px-4 py-0 text-5xl font-sans font-extrabold text-sky-200 drop-shadow-[0_1.2px_1.2px_rgba(0,0,1,0.9)]">Night Out</h1>
+                  <img className="max-h-20" src="/assets/images/logo/night-out-logo.png" alt="logo" />
                 </div>
                 <div className="hidden sm:ml-6 sm:flex sm:space-x-8 items-end">
                   <NavLink
                       to="/"
-                      className="inline-flex items-center px-2 py-2 text-xl font-bold text-gray-50 hover:text-sky-700 hover:text-2xl hover:drop-shadow-lg no-underline"
+                      className="inline-flex items-center px-2 py-2 text-xl font-bold text-indigo-900 hover:text-indigo-500 hover:text-2xl hover:drop-shadow-lg no-underline"
                       >
                         Book Now
                   </NavLink>
                   <NavLink
                       to="/about"
-                      className="inline-flex items-center px-2 py-2 text-xl font-bold text-gray-50 hover:text-sky-700 hover:text-2xl hover:drop-shadow-lg no-underline"
+                      className="inline-flex items-center px-2 py-2 text-xl font-bold text-indigo-900 hover:text-indigo-500 hover:text-2xl hover:drop-shadow-lg no-underline"
                       >
                         About
                   </NavLink>
                   <NavLink
                       to="/contact"
-                      className="inline-flex items-center px-2 py-2 text-xl font-bold text-gray-50 hover:text-sky-700 hover:text-2xl hover:drop-shadow-lg no-underline"
+                      className="inline-flex items-center px-2 py-2 text-xl font-bold text-indigo-900 hover:text-indigo-500 hover:text-2xl hover:drop-shadow-lg no-underline"
                       >
                         Contact
                   </NavLink>
                   <NavLink
                       to="/feedback"
-                      className="inline-flex items-center px-2 py-2 text-xl font-bold text-gray-50 hover:text-sky-700 hover:text-2xl hover:drop-shadow-lg no-underline"
+                      className="inline-flex items-center px-2 py-2 text-xl font-bold text-indigo-900 hover:text-indigo-500 hover:text-2xl hover:drop-shadow-lg no-underline"
                       >
                         Feedback
                   </NavLink>
@@ -47,9 +59,9 @@ export default function Navbar() {
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    <Menu.Button className="flex rounded-full bg-indigo-900 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                       <span className="sr-only">Open user menu</span>
-                      <StarIcon className="h-7 w-7 stroke-2 fill-blue-200 text-sky-700"/>
+                      <StarIcon className="h-7 w-7 stroke-2 fill-indigo-900 stroke-amber-100"/>
                     </Menu.Button>
                   </div>
                   <Transition
@@ -61,7 +73,7 @@ export default function Navbar() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-sky-200 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none no-underline">
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-amber-100 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none no-underline">
                       <Menu.Item>
                         {({ active }) => (
                           <a
@@ -69,8 +81,8 @@ export default function Navbar() {
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 no-underline')}
                           >
                              <div className= "flex flex-wrap content-center">
-                              <StarIcon className="h-7 w-7 stroke-2 fill-blue-200 text-sky-700"/>
-                              <p className="my-0 ml-2 pt-2 text-sky-700 text-lg leading-3">Artists</p>
+                              <StarIcon className="h-7 w-7 stroke-2 fill-amber-100 stroke-indigo-900"/>
+                              <p className="my-0 ml-2 pt-2 text-indigo-900 text-lg leading-3">Artists</p>
                              </div>
                           </a>
                         )}
@@ -82,8 +94,8 @@ export default function Navbar() {
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 no-underline')}
                           >
                             <div className= "flex flex-wrap content-center">
-                              <StarIcon className="h-7 w-7 stroke-2 fill-blue-200 text-sky-700"/>
-                              <p className="my-0 ml-2 pt-2 text-sky-700 text-lg leading-3 align-bottom">Restaurants</p>
+                              <StarIcon className="h-7 w-7 stroke-2 fill-amber-100 stroke-indigo-900"/>
+                              <p className="my-0 ml-2 pt-2 text-indigo-900 text-lg leading-3 align-bottom">Restaurants</p>
                             </div>
                           </a>
                         )}
