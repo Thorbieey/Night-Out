e8a8a2baf3ac609011d38ce4426d5d33953475c1
Anna Gilthorpe <annagilthorpe@gmail.com>
2023-03-22 16:38:30 +0000

    added and formatted footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BookNow from './components/pages/BookNow';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 import Feedback from './components/feedback/Feedback';
+import Footer from './components/navigation/Footer';
 import './App.css';
 
 function App() {
@@ -27,7 +28,8 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/feedback" element={<Feedback />} />
-        </Routes>          
+        </Routes> 
+        <Footer/>         
     </Router> 
   );
 }
