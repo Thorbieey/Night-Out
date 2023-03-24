6c80fd728aad87bc1d8a29556081f5f3565842d0
Anna Gilthorpe <annagilthorpe@gmail.com>
2023-03-24 12:38:22 +0000

    styling package slection fixes

--- src/components/content/PackageSelection.jsx
+++ src/components/content/PackageSelection.jsx
@@ -62,7 +62,7 @@ if (selection === "Gold Package") {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <h2 className="text-3xl font-bold tracking-tight text-amber-100 sm:text-4xl text-center">Build Your Night Out Package</h2>
-            <p className="mt-6 text-lg leading-8 text-white-600 text-center">
+            <p className="mt-6 text-lg leading-8 text-amber-100 text-center">
               Now you've chosen where you would like your night out to be, it's time to select the package that suits you.
             </p>
           </div>
@@ -71,11 +71,11 @@ if (selection === "Gold Package") {
             className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
           >
             {selections.map((selections,i) => (
-              <li key={i} className="bg-amber-100 rounded shadow md p-2">
+              <li key={i} className="mt-5 mb-2 flex flex-col items-center bg-amber-100 rounded shadow md p-2">
                 <img className="aspect-[3/2] w-full rounded-2xl object-cover" src={selections.imageUrl} alt="" />
                 <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-700 text-center">{selections.name}</h3>
                 <h5 className="mt-6 text-base font-semibold leading-8 tracking-tight text-gray-600 text-center">{selections.des}</h5>
-                <ul role="list" className="flex flex-col items-center">
+             
                 
             
                       
@@ -85,26 +85,18 @@ if (selection === "Gold Package") {
                 setSelections(selections.name)
                 // console.log(selections.name)
                }}
-               className="rounded-full bg-gray-600 py-2 px-3.5 text-sm uppercase font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+               className="mt-5 mb-2 rounded-full bg-gray-600 py-2 px-3.5 text-sm uppercase font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
              >
                Book now
              </button>
                   
-              </ul>
+             
               </li>
               
               
             ))}
           </ul>
         </div>
-      </div>
-      <div className="w-full h-full bg-no-repeat bg-cover"> 
-      <div style={{
-        width: '1500px',
-        height: '500px',
-        backgroundSize:'cover',
-        backgroundImage: 'url(" /assets/background-images/Band3.png")'
-      }}></div>
       </div>
       </>
     )
