b7e7bc9f8e6ee1feabc21e04f05c1b0360755b36
bertiemoogle <bertiemoogle@gmail.com>
2023-03-24 09:28:38 +0000

    Feedback form placeholder.

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 function Feedback(){
-    return(
-        <h1>Hello World</h1>
-    )
+    
+    return (
+        <div className="feedback-form">
+        <h1 className="headerFont">Feedback Form</h1>
+    
+        <div class="create-blog content">
+            <form action="/feedback" method="POST">
+                <input type="text" class="input-box" id="title" name="title" placeholder="Your title here..." required />
+                <input type="text" class="input-box" id="snippet" name="snippet" placeholder="A snippet of your feedback here..." required />
+                <textarea id="body" class="input-box" placeholder="Your feedback here..." name="body"></textarea>
+                <button className="btn custom-btn">Submit</button>
+            </form>
+        </div>
+    </div>
+    );
 }
 
-export default Feedback
+export default Feedback;
